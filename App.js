b7a1f8ca841ab4ebc2b512e9b23fe291d1ad330d
Manuel Alanís <manuelaag99@gmail.com>
2024-03-23 09:52:02 -0600

    start goal tracker app

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Pressable, ScrollView, StyleSheet, Text, View } from 'react-native';
+import AddNoteSection from './components/AddNoteSection';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<StatusBar style="auto" />
+			<AddNoteSection />
+			<View style={{ flex: 4, width: "100%", justifyContent: "center", alignItems: "center" }}>
+				<Text style={{ textAlign: "center", margin: 10 }}>
+					Mis metas
+				</Text>
+				<ScrollView>
+
+				</ScrollView>
+			</View>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
---
+++ components/AddNoteSection.jsx
@@ -0,0 +1,13 @@
+import { Pressable, Text, View } from "react-native";
+
+export default function AddNoteSection () {
+    return (
+        <View style={{ flex: 1, justifyContent: "center" }}>
+			<Pressable style={{ backgroundColor: "#f82", padding: 10, borderRadius: 6, margin: 16, width: "100%" }}>
+				<Text style={{ color: "#fff", fontWeight: "bold", marginHorizontal: 5 }}>
+					Agregar nueva meta
+				</Text>
+			</Pressable>
+		</View>
+    )
+}
