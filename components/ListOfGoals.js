d5ba6f0e45a3fe703ab9fdced7bbb8fc5d6c0b37
Manuel Alanís <manuelaag99@gmail.com>
2024-03-25 09:15:25 -0600

    add styling to goal list

--- components/ListOfGoals.js
+++ components/ListOfGoals.js
@@ -1,24 +1,29 @@
 import { useState } from "react";
-import { FlatList, Text, View } from "react-native";
+import { FlatList, Pressable, Text, View } from "react-native";
 
 export default function ListOfGoals ({ list }) {
-
+    function deleteItem (id) {
+        console.log(id)
+    }
     return (
-        <View style={{ flex: 4, width: "100%", justifyContent: "start", alignItems: "center" }}>
+        <View style={{ flex: 4, width: "100%", justifyContent: "start", alignItems: "center", padding: 10 }}>
 				{list && (list.length < 1) && <Text>
 					No has registrado ninguna meta.
 				</Text>}
-				{list && (list.length > 0) && <Text style={{ textAlign: "center", margin: 10 }}>
+				{list && (list.length > 0) && <Text style={{ textAlign: "center", margin: 16 }}>
 					Mis metas
 				</Text>}
 				{list && (list.length > 0) && <FlatList 
 					data={list} 
-					renderItem={({item}) => <View>
-						<Text>
-							{item.text}
-						</Text>
+					renderItem={({item}) => <View style={{ width: "100%", padding: 10, backgroundColor: "#f82", borderRadius: 8, marginTop: 10 }}>
+                        <Pressable onPressIn={() => deleteItem (item.id)}>
+                            <Text style={{ color: "#fff", width: "100%", textAlign: "center" }}>
+                                {item.text}
+                            </Text>
+                        </Pressable>
 					</View>} 
 					keyExtractor={item => item.id} 
+                    style={{ width: "100%" }}
 				/>}
 			</View>
     )
