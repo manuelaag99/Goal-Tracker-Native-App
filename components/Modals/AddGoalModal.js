9d4f26de8ad9f6549825c8aa64dbef5317dcedb8
Manuel Alanís <manuelaag99@gmail.com>
2024-03-26 21:33:00 -0600

    add template of state for pressing button

--- components/Modals/AddGoalModal.js
+++ components/Modals/AddGoalModal.js
@@ -8,28 +8,31 @@ export default function AddGoalModal ({ closeModal, enterNewGoal }) {
     }
 
     function addGoal () {
+        setIsButtonPressed(true);
         enterNewGoal(newGoal);
         setNewGoal({ text: "", id: "" });
         closeModal();
     }
+
+    const [isButtonPressed, setIsButtonPressed] = useState(false);
 
     return (
         <Modal animationType="slide">
             <View style={{ flex: 1, justifyContent: "center", alignItems: "center", padding: 30 }}>
                 <View style={{ flex: 1, justifyContent: "center", alignItems: "center", width: "100%" }}>
                     <View style={{ width: "100%", justifyContent: "center" }}>
-                        <TextInput style={{ width: "100%", padding: 10, borderWidth: 2, borderColor: "#777", borderRadius: 8 }} onChangeText={inputChangeHandle} value={newGoal.text} placeholder="Write down a goal here..." />
+                        <TextInput style={{ width: "100%", padding: 10, borderWidth: 2, borderColor: "#777", borderRadius: 8 }} onChangeText={inputChangeHandle} value={newGoal.text} placeholder="Escribe una meta aquí..." />
                     </View>
 
                     <View style={{ marginVertical: 10, width: "100%", flexDirection: "row", justifyContent: "space-between", alignItems: "flex-start", paddingHorizontal: 20 }}>
                         <Pressable style={{ width: "40%", backgroundColor: "#f82", borderRadius: 8 }} onPressIn={closeModal}>
                             <Text style={{ textAlign: "center", color: "#fff", fontWeight: "bold", padding: 10}}>
-                                Cancel
+                                Cancelar
                             </Text>
                         </Pressable>
-                        <Pressable style={{ width: "40%", backgroundColor: "#f82", borderRadius: 8 }} onPressIn={addGoal}>
+                        <Pressable style={[{ width: "40%", borderRadius: 8 }, (isButtonPressed ? { backgroundColor: "#333" } : { backgroundColor: "#f82" } ) ]} onPressIn={addGoal} onPressOut={() => setIsButtonPressed(false)}>
                             <Text style={{ textAlign: "center", color: "#fff", fontWeight: "bold", padding: 10}}>
-                                Add
+                                Agregar
                             </Text>
                         </Pressable>
                     </View>
